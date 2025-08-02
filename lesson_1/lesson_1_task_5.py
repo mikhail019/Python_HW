5bcbb73934e17b29e16e1a0b5840da6621505ad2
mikhail019 <shooter019@mail.ru>
2025-08-02 18:05:26 +0300

    lesson_1 исправление задачи 5

--- lesson_1/lesson_1_task_5.py
+++ lesson_1/lesson_1_task_5.py
@@ -1,7 +1,14 @@
 def print_number(num):
-    print(num, end="")
+    print(num, end='')
 
-for _ in range(11):
-    print_number(88005553535)
-
-print()
+print_number(8)
+print_number(8)
+print_number(0)
+print_number(0)
+print_number(5)
+print_number(5)
+print_number(5)
+print_number(3)
+print_number(5)
+print_number(3)
+print_number(5)
