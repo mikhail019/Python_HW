7c4b3ca79d6acf7e96a5727ed46582e2894cb93d
mikhail019 <shooter019@mail.ru>
2025-08-14 20:38:51 +0300

    lesson_3

---
+++ lesson_3/mailing.py
@@ -0,0 +1,6 @@
+class Mailing:
+    def __init__(self, to_address, from_address, cost, track):
+        self.to_address = to_address  # тип данных Address
+        self.from_address = from_address  # тип данных Address
+        self.cost = cost  # тип данных число
+        self.track = track  # тип данных строка
