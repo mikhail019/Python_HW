abc660b355cf2c83d04c512275b3a966d0261a83
mikhail019 <shooter019@mail.ru>
2025-09-14 01:02:03 +0300

    lesson_06k. Изменение в test_03_shop

--- lesson_06k/test_03_shop.py
+++ lesson_06k/test_03_shop.py
@@ -1,32 +1,58 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_shopping():
     driver = webdriver.Firefox()
-    driver.get("https://www.saucedemo.com/")
+    wait = WebDriverWait(driver, 10)  # Установка времени ожидания
 
-    # Авторизация
-    driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.XPATH, "//input[@type='submit']").click()
+    try:
+        driver.get("https://www.saucedemo.com/")
 
-    # Добавление товаров в корзину
-    driver.find_element(
-        By.XPATH, "//div[text()='Sauce Labs Backpack']"
-                  "/following-sibling::button").click()
-    driver.find_element(
-        By.XPATH, "//div[text()='Sauce Labs Bolt T-Shirt']"
-                  "/following-sibling::button").click()
-    driver.find_element(
-        By.XPATH, "//div[text()='Sauce Labs Onesie']"
-                  "/following-sibling::button").click()
+        # Авторизация
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        driver.find_element(By.ID, "login-button").click()
 
-    # Переход в корзину
-    driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
-    driver.find_element(By.XPATH, "//button[text()='Checkout']").click()
+        # Добавление товаров в корзину
+        driver.find_element(
+            By.ID, "add-to-cart-sauce-labs-backpack").click()
+        driver.find_element(
+            By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+        driver.find_element(
+            By.ID, "add-to-cart-sauce-labs-onesie").click()
 
-    # Заполнение формы
-    driver.find_element(By.NAME, "firstName").send_keys("Иван")
-    driver.find_element(By.NAME, "lastName").send_keys("Петров")
-    driver.find_element(By.NAME, "postalCode")
+        # Переход в корзину
+        driver.find_element(
+            By.XPATH, "//a[@class='shopping_cart_link']").click()
+        driver.find_element(
+            By.XPATH, "//button[text()='Checkout']").click()
+
+        # Заполнение формы
+        driver.find_element(
+            By.ID, "firstName").send_keys("Иван")
+        driver.find_element(
+            By.ID, "lastName").send_keys("Петров")
+        driver.find_element(
+            By.ID, "postalCode").send_keys("12345")
+
+        # Продолжение покупки
+        driver.find_element(By.ID, "continue").click()
+
+        # Завершение заказа
+        wait.until(EC.element_to_be_clickable((By.ID, "finish"))).click()
+
+        # Проверка успешного оформления
+        success_message = wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, "complete-header"))
+        )
+        assert "THANK YOU FOR YOUR ORDER" in success_message.text.upper()
+
+    finally:
+        driver.quit()
+
+
+# Запуск теста
+test_shopping()
